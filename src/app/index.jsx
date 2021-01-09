2d2f93cc504abdb57b5662b42474fe5a6f566e14
Tim Gonzalez <timg@Tims-MacBook-Pro.local>
2021-01-08 23:29:29 -0800

    Initial Redux setup

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import { store } from './store';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from '../app/components/Main';
+// import { Dashboard } from '../app/components/Dashboard';
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById("app")
+)
