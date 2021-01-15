a6eddafea3a43a6533a35ad3f82629deef8be752
timgonzo <tvgonz@gmail.com>
2021-01-14 21:57:53 -0800

    Implemented user authorization.

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
+import { Redirect } from 'react-router';
 import {Provider} from "react-redux";
 import {store} from "../store";
+import {ConnectedLogin} from './Login';
 import {ConnectedDashboard} from "./Dashboard";
 import {ConnectedNavigation} from "./Navigation";
 import {ConnectedTaskDetail} from "./TaskDetail";
+
+const RouteGuard = Component => ({match}) => {
+    console.info("Route guard", match);
+    if (!store.getState().session.authenticated) {
+        return <Redirect to="/" />
+    }
+    {
+        return <Component match={match}/>
+    }
+}
 
 export const Main = () => (
     <Provider store={store}>
         <BrowserRouter>
             <div>
                 <ConnectedNavigation/>
-                <Route exact path="/dashboard" render={() => <ConnectedDashboard/>}/>
-                <Route exact path="/task/:id" render={({match}) => (<ConnectedTaskDetail match={match}/>)}/>
+                <Route exact path="/" component={ConnectedLogin} />
+                <Route exact path="/dashboard" render={RouteGuard(ConnectedDashboard)}/>
+                <Route exact path="/task/:id" render={RouteGuard(ConnectedTaskDetail)}/>
             </div>
         </BrowserRouter>
     </Provider>
--- src/server/initialize-db.js
+++ src/server/initialize-db.js
@@ -1,11 +1,14 @@
-import { defaultState } from './defaultState';
-import { connectDB } from './connect-db';
+import {defaultState} from './defaultState';
+import {connectDB} from './connect-db';
 
-async function initializeDB(){
+async function initializeDB() {
     let db = await connectDB();
-    for (let collectionName in defaultState){
-        let collection = db.collection(collectionName);
-        await collection.insertMany(defaultState[collectionName]);
+    let user = await db.collection('users').findOne({id: "U1"});
+    if (!user) {
+        for (let collectionName in defaultState) {
+            let collection = db.collection(collectionName);
+            await collection.insertMany(defaultState[collectionName]);
+        }
     }
 }
 
